0ef3aa238907c2375d4dc83ca23cd142bc0c53d2
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2024-05-19 01:53:12 +0530

    Utils and hooks and etc

---
+++ src/hooks/useAuthentication.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useAuthentication = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthentication = () =>
+      setIsAuthenticated(!!localStorage.getItem('token'));
+
+    checkAuthentication();
+
+    window.addEventListener('storage', checkAuthentication);
+
+    return () => window.removeEventListener('storage', checkAuthentication);
+  }, []);
+
+  return isAuthenticated;
+};
+
+export default useAuthentication;
